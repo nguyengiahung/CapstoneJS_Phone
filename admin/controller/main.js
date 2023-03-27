1267df90aae9b1dea83527e80f724a2edafb0ff6
Thuong <vanthuong.mar@gmail.com>
2023-03-27 20:44:18 +0700

    update admin

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -7,8 +7,13 @@ function showTable(arrayData){
                 <td></td>
                 <td>${product.name}</td>
                 <td>${product.price.toLocaleString()}</td>
+                <td>${product.screen}</td>
+                <td>${product.backCamera}</td>
+                <td>${product.frontCamera}</td>
+                <td>${product.img}</td>
                 <td>${product.img}</td>
                 <td>${product.desc}</td>
+                <td>${product.type}</td>
                 <td>
                     <button class="btn btn-danger">Edit</button>
                     <button class="btn btn-info">Delete</button>
@@ -17,7 +22,7 @@ function showTable(arrayData){
             </tr>
         `
     });
-    document.querySelector('#btn').innerHTML = content;
+    document.querySelector('#tblSanPham').innerHTML = content;
 
 }
 //Hien thi danh sach 
@@ -29,9 +34,10 @@ function showProductsList(){
         showTable(result.data)
     })
     //reject
-    .catch(function(){
+    .catch(function(error){
         console.log(error);
     });
-//Lay danh sach thoi diem load web
-showProductsList
 }
+showProductsList();
+
+//Lay danh sach thoi diem load web
