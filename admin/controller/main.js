077307849bc6d4c9cba309d566c1b7ebda8c9b6f
Thuong <vanthuong.mar@gmail.com>
2023-03-30 14:16:01 +0700

    update main

--- admin/controller/main.js
+++ admin/controller/main.js
@@ -1,7 +1,8 @@
 const productSer = new productsServices();
-function showTable(arrayData){
+//Hien thi bang 
+function showTable(arrayData) {
     var content = "";
-    data.map(function(product,index){
+    arrayData.map(function (product, index) {
         content += `
             <tr>
                 <td></td>
@@ -10,34 +11,124 @@ function showTable(arrayData){
                 <td>${product.screen}</td>
                 <td>${product.backCamera}</td>
                 <td>${product.frontCamera}</td>
-                <td>${product.img}</td>
-                <td>${product.img}</td>
+                <td><img style="width: 100px" src="${product.img}" alt=""></td>
                 <td>${product.desc}</td>
                 <td>${product.type}</td>
                 <td>
-                    <button class="btn btn-danger">Edit</button>
-                    <button class="btn btn-info">Delete</button>
+                    <button data-toggle="modal" data-target="#myModal" onclick="showProductDetail('${product.id}')" class="btn btn-danger">Edit</button>
+                    <button onclick="deleteProduct('${product.id}')" class="btn btn-info">Delete</button>
                 </td>
-            
             </tr>
         `
     });
-    document.querySelector('#tblSanPham').innerHTML = content;
+    document.querySelector('#tblDanhSachSP').innerHTML = content;
 
 }
 //Hien thi danh sach 
-function showProductsList(){
+function showProductsList() {
     var axiosResult = productSer.getProductsList();
     //resolve
-    axiosResult.then(function(result){
+    axiosResult.then(function (result) {
         console.log(result.data);
         showTable(result.data)
     })
-    //reject
-    .catch(function(error){
-        console.log(error);
-    });
+        //reject
+        .catch(function (error) {
+            console.log(error);
+        });
 }
+//Lay danh sach thoi diem load web
 showProductsList();
 
-//Lay danh sach thoi diem load web
+function addProduct() {
+    // get data
+    var name = document.querySelector("#name").value;
+    var price = document.querySelector("#price").value;
+    var screen = document.querySelector("#screen").value;
+    var backCamera = document.querySelector("#backCamera").value;
+    var frontCamera = document.querySelector("#frontCamera").value;
+    var img = document.querySelector("#img").value;
+    var desc = document.querySelector("#desc").value;
+    var type = document.querySelector("#type").value;
+    //create object
+    var products = new Product(name, price, screen, backCamera, frontCamera, img, desc, type)
+    console.log(products);
+    //truyen xuong BE
+    productSer.addProductSer(products)
+        .then(function (result) {
+            console.log(result);
+            showProductsList();
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+    //hien thi danh sach san pham
+}
+
+document.querySelector("#addProduct").onclick = function () {
+    //add button for form
+    document.querySelector("#myModal .modal-footer").innerHTML = `
+        <button class="btn btn-success" onclick="addProduct()">Add</button>
+    `
+    document.querySelector("#formProduct").reset();
+}
+
+function deleteProduct(id) {
+    console.log(id);
+    productSer.deleteProductSer(id)
+        .then(function (result) {
+            console.log(result);
+            showProductsList();
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+//GET DATA
+function showProductDetail(id) {
+    console.log(id);
+    productSer.getProductItem(id)
+        .then(function (result) {
+            console.log(result.data);
+            document.querySelector("#name").value = result.data.name;
+            document.querySelector("#price").value = result.data.price;
+            document.querySelector("#screen").value = result.data.screen;
+            document.querySelector("#backCamera").value = result.data.backCamera;
+            document.querySelector("#frontCamera").value = result.data.frontCamera;
+            document.querySelector("#img").value = result.data.img;
+            document.querySelector("#desc").value = result.data.desc;
+            document.querySelector("#type").value = result.data.type;
+             //add button update product
+             document.querySelector("#myModal .modal-footer").innerHTML = `
+             <button class="btn btn-success" onclick="updateProduct('${result.data.id}')">Update Product</button>
+            `
+        })
+        .catch(function (error) {
+            console(error);
+        })
+}
+//PUT DATA
+function updateProduct(id){
+    console.log(id);
+    //Lay du lieu tu form
+    var name = document.querySelector("#name").value;
+    var price = document.querySelector("#price").value;
+    var screen = document.querySelector("#screen").value;
+    var backCamera = document.querySelector("#backCamera").value;
+    var frontCamera = document.querySelector("#frontCamera").value;
+    var img = document.querySelector("#img").value;
+    var desc = document.querySelector("#desc").value;
+    var type = document.querySelector("#type").value;
+    //Create object 
+    var productUpdate = new Product(name, price, screen, backCamera, frontCamera, img, desc, type)
+    console.log(productUpdate);
+    //Tuong tac voi BE
+    productSer.updateProductSer(productUpdate,id)
+    .then(function(result){
+        console.log(result.data);
+        showProductsList();
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
--- admin/services/productsServices.js
+++ admin/services/productsServices.js
@@ -9,4 +9,34 @@ function productsServices() {
     })
     
   }
+  //addProduct
+  this.addProductSer = function (products) {
+    return axios({
+      method: 'post',
+      url: "https://640d37241a18a5db8372cf66.mockapi.io/api/v1/PhoneJS" ,
+      data: products
+  })
+}
+  //DeleteProduct
+  this.deleteProductSer = function (id) {
+    return axios({
+      method: 'delete',
+      url: `https://640d37241a18a5db8372cf66.mockapi.io/api/v1/PhoneJS/${id}`,
+  })
+}
+  //GET
+this.getProductItem = function (id) {
+  return axios({
+    method: 'get',
+    url: `https://640d37241a18a5db8372cf66.mockapi.io/api/v1/PhoneJS/${id}`,
+  })
+}
+ //PUT
+ this.updateProductSer = function (productUpdate,id) {
+  return axios({
+    method: 'put',
+    url: `https://640d37241a18a5db8372cf66.mockapi.io/api/v1/PhoneJS/${id}`,
+    data: productUpdate
+  })
+}
 }
